537e5a1e0bc77a7458f5d2996f53262efff5e374
Jp <jpcosta_severo@hotmail.com>
2020-04-07 22:41:16 -0300

    Preparar para Login e Edit

--- backend/src/database/migrations/20200406195329_create_movies.js
+++ backend/src/database/migrations/20200406195329_create_movies.js
@@ -4,11 +4,12 @@ exports.up = function(knex) {
         table.string('id').primary();
         table.string('title').notNullable();
         table.string('description').notNullable();
-        table.string('genre').notNullable();
-        table.string('date').notNullable();
-        table.string('actors').notNullable();
+        table.string('genre');
+        table.string('date');
+        table.string('actors');
         table.string('url');
         table.string('img');
+        table.string('idUser');
       });
 };
 
